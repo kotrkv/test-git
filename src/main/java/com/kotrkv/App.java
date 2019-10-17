41e082b8138f005d3c2195fa97baf150d4eeb212
Reshetko K.V <reshetkokv@energomera.ru>
2019-10-17 13:51:06 +0300

    Change App
    * add System.out.println()

--- src/main/java/com/kotrkv/App.java
+++ src/main/java/com/kotrkv/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("From master branch after create secondBranch");
         String name = "";
         int i = 0;
         System.out.println( "Hello World!" );
